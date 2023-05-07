361c941a8a8d971869b5f3ba0dd3f5fc3a84bea4
adaniel <adaniel@digibank.co.il>
2023-05-07 16:41:03 +0300

    :tada: Auth0

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import {useUser} from "@auth0/nextjs-auth0/client";
 
 const Nav = () => {
+    const {user, isLoading} = useUser();
     return (
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center ">
@@ -8,12 +10,19 @@ const Nav = () => {
                     <Link href="/">
                         My Website
                     </Link>
+                    {user && <span className="text-sm text-gray-400"> - {`Hello ${user.name}`}</span>}
                 </div>
-                <ul className="flex space-x-4 text-white">
+                <ul className="flex items-center space-x-4 text-white">
                     <li className="hover:text-gray-200">
                         <Link href="/cart">
                             Cart
                         </Link>
+                    </li>
+                    <li className="hover:text-gray-200 border-2 border-indigo-600 p-2 rounded">
+                        {!user
+                            ? <a href="/api/auth/login">Login as Vendor</a>
+                            : <a href="/api/auth/logout">Logout</a>
+                        }
                     </li>
                 </ul>
             </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import Layout from '@/components/Layout'
 import {ApolloProvider} from "@apollo/client";
+import {UserProvider} from '@auth0/nextjs-auth0/client';
 import {client} from "../../gqlClient";
 
 export default function App({Component, pageProps}: AppProps) {
-    return <ApolloProvider client={client}>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </ApolloProvider>
+    return <UserProvider>
+        <ApolloProvider client={client}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider>
+    </UserProvider>
 }
